c814b590a267320a7bef265ba79c9a73a39b013e
Nigel T. Crowther <156706887+ncrowthe@users.noreply.github.com>
2026-06-29 13:02:02 +0100

    Add monster sprites

--- runbobrun.py
+++ runbobrun.py
@@ -1,6 +1,8 @@
 from tkinter import *
+from tkinter import messagebox
 import random
 import time
+from typing import Any
 
 MAX_TIME_SECS = 60
 
@@ -193,6 +195,13 @@ class StickFigureSprite(Sprite):
             if top and self.y < 0 and collided_top(co, sprite_co):
                 self.y = -self.y
                 top = False
+                if sprite.endgame:
+                    if isinstance(sprite, ToiletSprite):
+                        self.game.end_code = self.game.COMPLETED
+                        ## move to next level
+                    elif isinstance(sprite, MonsterSprite):
+                        self.game.end_code = self.game.EATEN
+                    self.game.running = False
             if bottom and self.y > 0 and collided_bottom(self.y, 
                     co, sprite_co):
                 self.y = sprite_co.y1 - co.y2
@@ -200,6 +209,12 @@ class StickFigureSprite(Sprite):
                     self.y = 0
                 bottom = False
                 top = False
+                if sprite.endgame:
+                    if isinstance(sprite, ToiletSprite):
+                        self.game.end_code = self.game.COMPLETED
+                    elif isinstance(sprite, MonsterSprite):
+                        self.game.end_code = self.game.EATEN
+                    self.game.running = False
             if bottom and falling and self.y == 0 \
                     and co.y2 < self.game.canvas_height \
                     and collided_bottom(1, co, sprite_co):
@@ -208,11 +223,19 @@ class StickFigureSprite(Sprite):
                 self.x = 0
                 left = False
                 if sprite.endgame:
+                    if isinstance(sprite, ToiletSprite):
+                        self.game.end_code = self.game.COMPLETED
+                    elif isinstance(sprite, MonsterSprite):
+                        self.game.end_code = self.game.EATEN
                     self.game.running = False
             if right and self.x > 0 and collided_right(co, sprite_co):
                 self.x = 0
                 right = False
                 if sprite.endgame:
+                    if isinstance(sprite, ToiletSprite):
+                        self.game.end_code = self.game.COMPLETED
+                    elif isinstance(sprite, MonsterSprite):
+                        self.game.end_code = self.game.EATEN
                     self.game.running = False
 
         if falling and bottom and self.y == 0 \
@@ -221,17 +244,75 @@ class StickFigureSprite(Sprite):
         self.game.canvas.move(self.image, self.x, self.y)
 
 class ToiletSprite(Sprite):
-    def __init__(self, game, photo_image, image_wet, x, y, width, height):
+    def __init__(self, game, photo_image, image_wet, x, y, y_movement, width, height):
         Sprite.__init__(self, game)
         self.photo_image = photo_image
         self.image_wet = image_wet
+        self.y_movement = y_movement
         self.image = game.canvas.create_image(x, y, 
                 image=self.photo_image, anchor='nw')
         self.coordinates = Coords(x, y, x + (width / 2), y + height)
+        self.y_delta = 1
+        self.y_count = 0
+        self.endgame = True        
+
+    def move(self):
+        if self.y_count >= self.y_movement:
+            self.y_delta = -1
+        elif self.y_count <= 0:
+            self.y_delta = 1
+        self.y_count += self.y_delta
+        self.game.canvas.move(self.image, 0, self.y_delta)
+        self.coordinates.y1 += self.y_delta
+        self.coordinates.y2 += self.y_delta
+
+class MonsterSprite(Sprite):
+    def __init__(self, game, photo_image, x, y, x_movement, y_movement, width, height):
+        Sprite.__init__(self, game)
+        self.photo_image = photo_image
+        self.x_movement = x_movement        
+        self.y_movement = y_movement
+        self.image = game.canvas.create_image(x, y, 
+                image=self.photo_image, anchor='nw')
+        self.coordinates = Coords(x, y, x + (width / 2), y + height)
+
+        self.x_delta = 1
+        self.x_count = 0
+
+        self.y_delta = 1
+        self.y_count = 0        
+
         self.endgame = True
 
+    def move(self):
+        if self.y_count >= self.y_movement:
+            self.y_delta = -2
+        elif self.y_count <= 0:
+            self.y_delta = 2
+        self.y_count += self.y_delta
+
+        if self.x_count >= self.x_movement:
+            self.x_delta = -2
+        elif self.x_count <= 0:
+            self.x_delta = 2
+        self.x_count += self.x_delta
+
+        self.game.canvas.move(self.image, self.x_delta, self.y_delta)
+
+        self.coordinates.x1 += self.x_delta
+        self.coordinates.x2 += self.x_delta     
+
+        self.coordinates.y1 += self.y_delta
+        self.coordinates.y2 += self.y_delta            
+
 class Game:
+
+    TIME_OUT = 1
+    EATEN = 2
+    COMPLETED = 3
+
     def __init__(self):
+        self.level = 1
         self.tk = Tk()
         self.tk.title('Run Bob Run')
         self.tk.resizable(0, 0)
@@ -242,6 +323,72 @@ class Game:
         self.tk.update()
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+    
+    def play(self)  :
+        elapsed = time.time() - self.start_time
+        remaining = max(0, MAX_TIME_SECS - int(elapsed))
+        self.canvas.itemconfig(self.timer_text, text=str(remaining))
+        if elapsed > MAX_TIME_SECS:
+            self.running = False
+            self.end_code = self.TIME_OUT
+        for sprite in self.sprites:
+            sprite.move()
+
+    def gameOver(self):
+        for sprite in self.sprites:
+            if isinstance(sprite, ToiletSprite):
+                self.canvas.itemconfig(sprite.image,
+                        image=sprite.image_wet)
+
+            if isinstance(sprite, StickFigureSprite):
+                self.canvas.itemconfig(sprite.image,
+                        image=sprite.figure_wet)
+
+            self.tk.update_idletasks()
+            self.tk.update()                                
+
+        if (self.end_code == self.COMPLETED):
+            self.canvas.create_text(250, 380, text="Well Done!",
+                    font=('Helvetica', 30), fill='blue')
+
+        if (self.end_code == self.EATEN):
+            self.canvas.create_text(250, 380, text="You've been eaten!",
+                    font=('Helvetica', 30), fill='red')
+
+        if (self.end_code == self.TIME_OUT):
+            self.canvas.create_text(250, 380, text="Time out!",
+                    font=('Helvetica', 30), fill='red')                                        
+
+
+        self.tk.update_idletasks()
+        self.tk.update()
+        time.sleep(3)
+
+    def mainloop(self):
+        while True:
+            if self.running == True:
+                self.play()
+            else:
+                self.gameOver()
+
+                self.newGame(   (self.end_code == self.COMPLETED))
+                break
+            self.tk.update_idletasks()
+            self.tk.update()
+            time.sleep(0.02)
+
+    def newGame(self, incrementLevel):
+
+        self.sprites = []
+        self.end_code = self.COMPLETED
+        self.running = True
+        self.start_time = time.time()
+
+        if (incrementLevel):
+            self.level = self.level + 1
+
+        self.tk.title('Run Bob Run Level ' + str(self.level))
+
         self.bg = PhotoImage(file='icons/background.gif')
         w = self.bg.width()
         h = self.bg.height()
@@ -250,79 +397,62 @@ class Game:
                 self.canvas.create_image(x * w, y * h, 
                         image=self.bg, anchor='nw')
         self.sprites = []
-        self.running = True
-        self.start_time = time.time()
+        self.end_code = self.COMPLETED
         self.timer_text = self.canvas.create_text(490, 10,
-                text=str(MAX_TIME_SECS), font=('Helvetica', 18), fill='white', anchor='ne')
+            text=str(MAX_TIME_SECS), font=('Helvetica', 18), fill='white', anchor='ne')        
 
-    def mainloop(self):
-        while True:
-            if self.running == True:
-                elapsed = time.time() - self.start_time
-                remaining = max(0, MAX_TIME_SECS - int(elapsed))
-                self.canvas.itemconfig(self.timer_text, text=str(remaining))
-                if elapsed > MAX_TIME_SECS:
-                    self.running = False
-                    for sprite in self.sprites:
-                        if isinstance(sprite, StickFigureSprite):
-                            self.canvas.itemconfig(sprite.image,
-                                    image=sprite.figure_wet)
-                    self.canvas.create_text(250, 250,
-                            text='Oh dear, you did not make it!',
-                            font=('Helvetica', 18), fill='red')
-                    self.tk.update_idletasks()
-                    self.tk.update()
-                    time.sleep(5)
-                    break
-                for sprite in self.sprites:
-                    sprite.move()
-            else:
-                for sprite in self.sprites:
-                    if isinstance(sprite, ToiletSprite):
-                        self.canvas.itemconfig(sprite.image,
-                                image=sprite.image_wet)
+        p1a =  PhotoImage(file='icons/platform1a.gif')
+        p1b =  PhotoImage(file='icons/platform1b.gif')
+        p2a =  PhotoImage(file='icons/platform2a.gif')
+        p2b =  PhotoImage(file='icons/platform2b.gif')
+        p3a =  PhotoImage(file='icons/platform3a.gif')
+        p3b =  PhotoImage(file='icons/platform3b.gif')
 
-                self.canvas.create_text(250, 380, text='Well Done!',
-                        font=('Helvetica', 30), fill='red')
-                self.tk.update_idletasks()
-                self.tk.update()
-                time.sleep(5)
-                break
-            self.tk.update_idletasks()
-            self.tk.update()
-            time.sleep(0.02)
+        MAX_RIGHT = 12
+        #, game, photo_image1, photo_image2, x, y, width, height
+        platform10 = PlatformSprite(game=self, photo_image1=p2a, photo_image2=p2b, x = 45, y=60, width=60, height=10)
+        platform9 = PlatformSprite(self, p2a, p2b,  100, 120, 60, 10)                           
+        platform8 = PlatformSprite(self, p3a, p3b,  200 , 160, 32, 10)
+        platform7 = PlatformSprite(self, p2a, p2b, 260 , 200, 60, 10)
+        platform6 = PlatformSprite(self, p2a, p2b, 170 , 250, 60, 10)        
+        platform5 = PlatformSprite(self, p1a, p1b, 50 , 300, 100, 10)   
+        platform4 = PlatformSprite(self, p1a, p1b, 175 , 350, 100, 10)  
+        platform3 = PlatformSprite(self, p2a, p2b, 300 , 400, 60, 10)                               
+        platform2 = PlatformSprite(self, p1a, p1b, 150 , 440, 100, 10)                                                
+        platform1 = PlatformSprite(self, p2a, p2b, 30 , 480, 60, 10)                                                                       
+        self.sprites.append(platform1)
+        self.sprites.append(platform2)
+        self.sprites.append(platform3)
+        self.sprites.append(platform4)
+        self.sprites.append(platform5)
+        self.sprites.append(platform6)
+        self.sprites.append(platform7)
+        self.sprites.append(platform8)
+        self.sprites.append(platform9)
+        self.sprites.append(platform10)
+
+        toilet: ToiletSprite = ToiletSprite(self, PhotoImage(file='icons/toilet-empty.gif'), PhotoImage(file='icons/toilet-fill.gif'), 8, 30,  80, 27, 30)
+        self.sprites.append(toilet)
+
+        if (self.level > 1):
+            monster: MonsterSprite = MonsterSprite(self, PhotoImage(file='icons/monster1a.gif'), 20,    200, 30, 320, 30, 35)
+            self.sprites.append(monster)
+
+        if (self.level > 2):
+            monster2: MonsterSprite = MonsterSprite(self, PhotoImage(file='icons/monster1a.gif'), 30,   80, 90, 380, 30, 35)
+            self.sprites.append(monster2)        
+
+        if (self.level > 3):
+            monster3: MonsterSprite = MonsterSprite(self, PhotoImage(file='icons/monster1a.gif'), 40,  200, 180, 300, 30, 35)
+            self.sprites.append(monster3)     
+
+        if (self.level > 4):
+            monster4: MonsterSprite = MonsterSprite(self, PhotoImage(file='icons/monster1a.gif'), 60, 240, 120, 320, 30, 35)
+            self.sprites.append(monster4)                           
+
+        sf = StickFigureSprite(self)
+        self.sprites.append(sf)
+        self.mainloop()
 
 g = Game()
-
-p1a =  PhotoImage(file='icons/platform1a.gif')
-p1b =  PhotoImage(file='icons/platform1b.gif')
-p2a =  PhotoImage(file='icons/platform2a.gif')
-p2b =  PhotoImage(file='icons/platform2b.gif')
-p3a =  PhotoImage(file='icons/platform3a.gif')
-p3b =  PhotoImage(file='icons/platform3b.gif')
-
-platform10 = PlatformSprite(g, p2a, p2b, 45, 60, 60, 10)
-platform9 = PlatformSprite(g, p2a, p2b,  100, 120, 60, 10)                           
-platform8 = PlatformSprite(g, p3a, p3b,  200, 160, 32, 10)
-platform7 = PlatformSprite(g, p2a, p2b, 260, 200, 60, 10)
-platform6 = PlatformSprite(g, p2a, p2b, 170, 250, 60, 10)        
-platform5 = PlatformSprite(g, p2a, p2b, 50, 300, 60, 10)   
-platform4 = PlatformSprite(g, p1a, p1b, 175, 350, 100, 10)  
-platform3 = PlatformSprite(g, p2a, p2b, 300, 400, 60, 10)                               
-platform2 = PlatformSprite(g, p1a, p1b, 150, 440, 100, 10)                                                
-platform1 = PlatformSprite(g, p2a, p2b, 30, 480, 60, 10)                                                                       
-g.sprites.append(platform1)
-g.sprites.append(platform2)
-g.sprites.append(platform3)
-g.sprites.append(platform4)
-g.sprites.append(platform5)
-g.sprites.append(platform6)
-g.sprites.append(platform7)
-g.sprites.append(platform8)
-g.sprites.append(platform9)
-g.sprites.append(platform10)
-toilet = ToiletSprite(g, PhotoImage(file='icons/toilet-empty.gif'), PhotoImage(file='icons/toilet-fill.gif'),45, 30, 40, 35)
-g.sprites.append(toilet)
-sf = StickFigureSprite(g)
-g.sprites.append(sf)
-g.mainloop()
+g.newGame(True)        
